f5c2a15b4b26ccb3c79932748ee6c7baf4e7c563
Ruchika Nagarkar <ruchikanagarkar@outlook.com>
2017-06-05 18:46:33 +0530

    iffe added

--- js/script.js
+++ js/script.js
@@ -1,41 +1,54 @@
 
-var downloadImage = function(){
-	html2canvas(document.body, {
-	onrendered: function(canvas) {
-	  var a = document.createElement('a');
-	  a.href = canvas.toDataURL("image/png");
-	  a.download = $.now()+'.png';
-	  a.click();
-	},
-	allowTaint:true
-	});
-}
+(function(){
 
-var downloadPDF = function(){
-	html2canvas(document.body, {
-	    onrendered: function(canvas) {
-	    	/*var ctx = canvas.getContext('2d');
-			ctx.clearRect( 0 , 0 , canvas.width, canvas.height );
-			ctx.fillStyle="#FFFFFF";
-			ctx.fillRect(0 , 0 , canvas.width, canvas.height);
-			*/
-	        var imgData = canvas.toDataURL("image/png");
-	        var pdf = new jsPDF('p', 'mm', 'a4');
-	        // window.open(imgData);
-	        pdf.setProperties({
-				title: 'Screenshot Application',
-				creator: 'Ruchika Nagarkar'
-			});
-			//window.open(imgData);
-
-	        //pdf.addImage(imgData, 'PNG', 5, 5);
-	        //pdf.save($.now()+'.pdf');
-	        pdf.addHTML(document.body, function() {
-			  	pdf.save($.now()+'.pdf');
-			});
-	    }
+	document.getElementById('downloadImage').addEventListener('click', function(){
+		downloadImage();
 	});
-}
+
+	document.getElementById('downloadPDF').addEventListener('click', function(){
+		downloadImage();
+	});
+
+
+	var downloadImage = function(){
+		html2canvas(document.body, {
+		onrendered: function(canvas) {
+		  var a = document.createElement('a');
+		  a.href = canvas.toDataURL("image/png");
+		  a.download = $.now()+'.png';
+		  a.click();
+		},
+		allowTaint:true
+		});
+	}
+
+	var downloadPDF = function(){
+		html2canvas(document.body, {
+		    onrendered: function(canvas) {
+		    	/*var ctx = canvas.getContext('2d');
+				ctx.clearRect( 0 , 0 , canvas.width, canvas.height );
+				ctx.fillStyle="#FFFFFF";
+				ctx.fillRect(0 , 0 , canvas.width, canvas.height);
+				*/
+		        var imgData = canvas.toDataURL("image/png");
+		        var pdf = new jsPDF('p', 'mm', 'a4');
+		        // window.open(imgData);
+		        pdf.setProperties({
+					title: 'Screenshot Application',
+					creator: 'Ruchika Nagarkar'
+				});
+				//window.open(imgData);
+
+		        //pdf.addImage(imgData, 'PNG', 5, 5);
+		        //pdf.save($.now()+'.pdf');
+		        pdf.addHTML(document.body, function() {
+				  	pdf.save($.now()+'.pdf');
+				});
+		    }
+		});
+	}
+
+}());
 
 
 
